287cb6643ef2fb1399f7c0599bf18a34baca0842
Mahiamk <mahikomohammed@gmail.com>
2025-03-08 13:53:36 +0800

    AboutCard updated

--- nuundigital/src/components/About/About.jsx
+++ nuundigital/src/components/About/About.jsx
@@ -10,27 +10,13 @@ function About() {
     <Container fluid className="about-section">
       <Particle />
       <Container>
-        <Row style={{ justifyContent: "center", padding: "10px" }}>
-          <Col
-            md={7}
-            style={{
-              justifyContent: "center",
-              paddingTop: "30px",
-              paddingBottom: "50px",
-            }}
-          >
-            <h1 style={{ fontSize: "2.1em", paddingBottom: "20px" }}>
+        <Row>
+          <Col>
+            <h1 style={{ fontSize: "2.1em"}}>
               Our Quality<strong className="purple"> Services</strong>
             </h1>
           </Col>
             <Aboutcard />
-            
-          <Col
-            md={5}
-            style={{ paddingTop: "120px", paddingBottom: "50px" }}
-            className="about-img"
-          >
-          </Col>
           <Map />
         </Row>
       </Container>
