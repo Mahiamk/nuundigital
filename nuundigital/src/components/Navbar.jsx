5a2184e382e5fd0009ed4d254f2e36fd59d1397b
Mahiamk <mahikomohammed@gmail.com>
2025-03-16 21:15:12 +0800

    unnecessary page removed

--- nuundigital/src/components/Navbar.jsx
+++ nuundigital/src/components/Navbar.jsx
@@ -79,7 +79,7 @@ function NavBar() {
               </Nav.Link>
             </Nav.Item>
 
-            <Nav.Item>
+            {/* <Nav.Item>
               <Nav.Link
                 as={Link}
                 to="/blogs"
@@ -87,7 +87,7 @@ function NavBar() {
               >
                 <CgFileDocument style={{ marginBottom: "2px" }} /> Blogs
               </Nav.Link>
-            </Nav.Item>
+            </Nav.Item> */}
             <Nav.Item>
               <Nav.Link
                 as={Link}
